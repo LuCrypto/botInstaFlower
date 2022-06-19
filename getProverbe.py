1ec52f59b4c110ea9902f09670f61c8a45e6669a
JAGER Luc <lucjager67@gmail.com>
2022-06-20 00:00:35 +0200

    get image + get proverbe + premiere version qui marche 1.0

--- getProverbe.py
+++ getProverbe.py
@@ -28,33 +28,50 @@ with open(fichierInfos, 'r') as myFile:
     monJson = json.loads(myFile.read())
 
 # =============================================================================
-proverbes = driver.find_elements(By.CLASS_NAME, "sit")
 
-for i in range(len(proverbes)):
-    blockquote = proverbes[i].find_element(By.CSS_SELECTOR, "blockquote")
-    blockquote = blockquote.find_element(By.CSS_SELECTOR, "a")
+nombre_page = 11
+for j in range(nombre_page):
+    proverbes = driver.find_elements(By.CLASS_NAME, "sit")
 
-    nombre = proverbes[i].find_elements(By.CSS_SELECTOR, "span")
+    for i in range(len(proverbes)):
+        blockquote = proverbes[i].find_element(By.CSS_SELECTOR, "blockquote")
+        blockquote = blockquote.find_element(By.CSS_SELECTOR, "a")
 
-    if (len(nombre) == 2):
-        span = proverbes[i].find_element(By.CSS_SELECTOR, "span")
-        auteur = span.get_attribute("innerHTML")
-    else:
-        html_a = proverbes[i].find_elements(By.CSS_SELECTOR, "a")
-        auteur = html_a[1].get_attribute("innerHTML")
+        nombre = proverbes[i].find_elements(By.CSS_SELECTOR, "span")
 
-    print("nombre : ", len(nombre))
-    print("taille : ", blockquote.get_attribute("innerHTML"))
-    print("taille : ", auteur)
+        if (len(nombre) == 2):
+            span = proverbes[i].find_element(By.CSS_SELECTOR, "span")
+            auteur = span.get_attribute("innerHTML")
+        else:
+            html_a = proverbes[i].find_elements(By.CSS_SELECTOR, "a")
+            auteur = html_a[1].get_attribute("innerHTML")
 
-    # Save in json
-    monObjet = {
-        "texte": blockquote.get_attribute("innerHTML"),
-        "auteur":  auteur
-    }
-    monJson['proverbes'].append(monObjet)
+        print("nombre : ", len(nombre))
+        print("taille : ", blockquote.get_attribute("innerHTML"))
+        print("taille : ", auteur)
+
+        # Save in json
+        monObjet = {
+            "texte": blockquote.get_attribute("innerHTML"),
+            "auteur":  auteur,
+            "used": False
+        }
+        monJson['proverbes'].append(monObjet)
+    
+    # On passe à la page suivante
+    element_nav = driver.find_element(By.CLASS_NAME, "filter")
+    tableau_pages = element_nav.find_elements(By.CSS_SELECTOR, "li")
+    try:
+        nouvelle_page = tableau_pages[j+1].find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+    except:
+        break
+    driver.get(nouvelle_page)
+
 # =============================================================================
 
 # On enregistre le résultat
 with open(fichierInfos, 'w') as myFile:
     myFile.write(json.dumps(monJson))
+
+print("====================FIN")
+print("Nombre proverbes : ", len(monJson['proverbes']))
--- postOnInstagram.py
+++ postOnInstagram.py
@@ -1,87 +1,164 @@
-from typing import KeysView
-from cv2 import log
+from numpy import true_divide
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 import json
 import os
+from random import randrange
 
-# Initialisation
-# =============================================================================
-url = "https://www.instagram.com/"
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-WINDOW_SIZE = "1080,1080"
-# mon_image = "images/image_1.png"
+# Fonction permettant de récupérer un ensemble de tags prédéfinis
+def getTags():
+    nomFichier = "differentsTags.txt"
+    with open(nomFichier, 'r') as myFile:
+        content = myFile.read()
+        tableau_tags = content.split("\n")
+        print("longueur : ", len(tableau_tags))
 
-# =============================================================================
-# Configuration
-chrome_options = Options()
-# chrome_options.add_argument("--headless")
+    nombre_tags = randrange(20,len(tableau_tags))
+    nombre_a_enlever = len(tableau_tags) - nombre_tags
 
-chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-driver = webdriver.Chrome(executable_path=PATH, chrome_options=chrome_options)
+    print("tableau_tags : ", tableau_tags)
+    for i in range(nombre_a_enlever):
+        indice_random = randrange(0,len(tableau_tags))
+        del tableau_tags[indice_random]
+    
+    resultat_string = ' '.join(tableau_tags)
+    print("resultat_string : ", resultat_string)
 
-driver.implicitly_wait(10)
+    return resultat_string
 
-# Ouvrir la page
-driver.get(url)
+# Fonction permettant de récupérer une citation avec son auteur
+def getCitations():
+    nomFichier = "infosProverbes.json"
+    with open(nomFichier, 'r') as myFile:
+        monjson = json.loads(myFile.read())
+        tableau_citations = monjson['proverbes']
 
-# On accepte les cookies
-driver.find_element(By.CLASS_NAME, "HoLwm").click()
+        element_valide = False
+        while(not(element_valide)):
+            indice_random = randrange(0,len(tableau_citations))
+            if (not(tableau_citations[indice_random]['used'])):
+                element_valide = True
 
-# On récupère login + password pour l'authentification
-tableau = driver.find_elements(By.CLASS_NAME, "zyHYP")
+        tableau_citations[indice_random]['used'] = True
+    
+    # Mise à jour used
+    with open(nomFichier, 'w') as myFile:
+        myFile.write(json.dumps(tableau_citations))
 
-login = tableau[0]
-login.send_keys("lucjager67@gmail.com")
+    return tableau_citations[indice_random]['texte'] + "\n" + tableau_citations[indice_random]['auteur']
 
-password = tableau[1]
-password.send_keys("j14EDCsTeiNlDHvIXTFq")
+# Fonction permettant de récupérer le chemin d'une image enregistrée au préalable
+def getImage():
+    nomFichier = "infosEnregistrement.json"
+    with open(nomFichier, 'r') as myFile:
+        monjson = json.loads(myFile.read())
+        tableau_images = monjson['images']
 
-# Bouton connection
-driver.find_element(By.CLASS_NAME, "CovQj").click()
+        element_valide = False
+        while(not(element_valide)):
+            indice_random = randrange(0,len(tableau_images))
+            if (not(tableau_images[indice_random]['used'])):
+                element_valide = True
 
-# On attends 10 secondes
-time.sleep(10)
+        tableau_images[indice_random]['used'] = True
+    
+    # Mise à jour used
+    with open(nomFichier, 'w') as myFile:
+        myFile.write(json.dumps(monjson))
 
-# Turn off notifications
-driver.find_element(By.CLASS_NAME, "_a9_1").click()
+    chemin = os.path.abspath("images/" + tableau_images[indice_random]['nomImage'])
 
-# On récupère le bouton pour ajouter une publication
-tableau_boutons = driver.find_elements(By.CLASS_NAME, "_ab6-")
-ajouter_publication = tableau_boutons[3]
-ajouter_publication.click()
+    tags_bonus = ""
+    for i in range(len(tableau_images[indice_random]['tags'])):
+        tags_bonus += "#" + tableau_images[indice_random]['tags'][i] + " "
+    
+    print("chemin : ", chemin)
+    print("tags_bonus : ", tags_bonus)
 
-# elements = driver.find_elements(By.CLASS_NAME, "_aba7")
-# div_selectionner = elements[1]
+    return chemin, tags_bonus, tableau_images[indice_random]['source']
 
-# 0 -> ne fait rien
-# 1 -> Change la photo de profil
-# 2 -> Mets le texte dans la barred de recherche
-# 3 -> Mets l'image pour la publication -> trouvé !
-# On envoie l'image
-tableau_input = driver.find_elements(By.XPATH, "//input")
-tableau_input[3].send_keys(
-    os.path.abspath("images/image_24.jpg"))
+# Permet de poster une image sur instagram
+def posterImage():
+    # Initialisation
+    # =============================================================================
+    url = "https://www.instagram.com/"
+    PATH = "C:\Program Files (x86)\chromedriver.exe"
+    WINDOW_SIZE = "1080,1080"
 
-# On clique sur next
-div_bouton_next = driver.find_element(By.CLASS_NAME, "_abaa")
-bouton_next = div_bouton_next.find_element(By.CLASS_NAME, "_acan")
-bouton_next.click()
+    # =============================================================================
+    # Configuration
+    chrome_options = Options()
+    # chrome_options.add_argument("--headless")
 
-# Filters/Adjustements
-# TODO
+    chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+    driver = webdriver.Chrome(executable_path=PATH, chrome_options=chrome_options)
 
-# On clique sur next
-div_bouton_next = driver.find_element(By.CLASS_NAME, "_abaa")
-bouton_next = div_bouton_next.find_element(By.CLASS_NAME, "_acan")
-bouton_next.click()
+    driver.implicitly_wait(10)
 
-# Permet de mettre un commentaire au post ainsi que les hashtags
-driver.find_element(By.CLASS_NAME, "_ablz").send_keys("I am caption #flowers")
+    # Ouvrir la page
+    driver.get(url)
 
-# On clique sur share
-div_bouton_next = driver.find_element(By.CLASS_NAME, "_abaa")
-bouton_next = div_bouton_next.find_element(By.CLASS_NAME, "_acan")
-bouton_next.click()
+    # On accepte les cookies
+    driver.find_element(By.CLASS_NAME, "HoLwm").click()
+
+    # On récupère login + password pour l'authentification
+    tableau = driver.find_elements(By.CLASS_NAME, "zyHYP")
+    login = tableau[0]
+    login.send_keys("lucjager67@gmail.com")
+    password = tableau[1]
+    password.send_keys("j14EDCsTeiNlDHvIXTFq")
+
+    # Bouton connection
+    driver.find_element(By.CLASS_NAME, "CovQj").click()
+
+    # On attends 10 secondes
+    time.sleep(10)
+
+    # Turn off notifications
+    driver.find_element(By.CLASS_NAME, "_a9_1").click()
+
+    # On récupère le bouton pour ajouter une publication
+    driver.find_elements(By.CLASS_NAME, "_ab6-")[3].click()
+
+    # 0 -> ne fait rien
+    # 1 -> Change la photo de profil
+    # 2 -> Mets le texte dans la barred de recherche
+    # 3 -> Mets l'image pour la publication -> trouvé !
+    # On envoie l'image
+    chemin_image, tags_bonus, source_image = getImage()
+    tableau_input = driver.find_elements(By.XPATH, "//input")
+    tableau_input[3].send_keys(chemin_image)
+
+    # On clique sur next
+    div_bouton_next = driver.find_element(By.CLASS_NAME, "_abaa")
+    bouton_next = div_bouton_next.find_element(By.CLASS_NAME, "_acan")
+    bouton_next.click()
+
+    # Filters/Adjustements
+    # TODO
+
+    # On clique sur next
+    div_bouton_next = driver.find_element(By.CLASS_NAME, "_abaa")
+    bouton_next = div_bouton_next.find_element(By.CLASS_NAME, "_acan")
+    bouton_next.click()
+
+    citation = getCitations()
+    tags = getTags()
+    description = citation + "\n\n" + "Source : " + source_image + "\n\n" + tags + " " + tags_bonus
+    # Permet de mettre un commentaire au post ainsi que les hashtags
+    driver.find_element(By.CLASS_NAME, "_ablz").send_keys(description)
+
+    # On clique sur share
+    div_bouton_next = driver.find_element(By.CLASS_NAME, "_abaa")
+    bouton_next = div_bouton_next.find_element(By.CLASS_NAME, "_acan")
+    bouton_next.click()
+    time.sleep(15)
+
+while (True):
+    posterImage()
+    # 30 min
+    time.sleep(1800)
+
+
